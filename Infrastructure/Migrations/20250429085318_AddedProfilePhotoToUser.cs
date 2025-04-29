0f752e64f19862deb1eee2aaec8f841a82faded6
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-04-29 12:42:10 +0300

    db migration for user profile pic

---
+++ Infrastructure/Migrations/20250429085318_AddedProfilePhotoToUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedProfilePhotoToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PictureUrl",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PictureUrl",
+                table: "AspNetUsers");
+        }
+    }
+}
