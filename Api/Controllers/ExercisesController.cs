6604f5a7396040458c94274ef7e3ec9af189f71c
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-04-30 23:04:22 +0300

    exercises controller bug fix

--- Api/Controllers/ExercisesController.cs
+++ Api/Controllers/ExercisesController.cs
@@ -1,9 +1,13 @@
 using Application.Abstracts;
 using Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
 
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
 public class ExercisesController : Controller<Exercise>
 {
     public ExercisesController(IRepository<Exercise> repository) : base(repository)
