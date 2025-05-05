8bb0d8c70e4ab77a60c2ef39164439b3b766d380
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-06 00:06:34 +0300

    add steps controller

---
+++ Api/Controllers/StepsInfoController.cs
@@ -0,0 +1,32 @@
+using Application.Abstracts.IServices;
+using Application.DTOs.StepsInfo;
+using Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+[Authorize]
+public class StepsInfoController : Controller<StepsInfoReadDto, StepsInfoCreateDto, StepsInfoUpdateDto, StepsInfo>
+{
+    private new readonly IStepsInfoService _service;
+
+    public StepsInfoController(IStepsInfoService service) : base(service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("get-by-userId-and-period/{userId:guid}")]
+    public async Task<ActionResult<IEnumerable<StepsInfoReadDto>>> GetByUserIdAndPeriod(Guid userId,
+        [FromQuery] DateTime fromDate,
+        [FromQuery] DateTime toDate)
+    {
+        var steps = await _service.GetStepsInfoByUserIdAndPeriod(userId, fromDate, toDate);
+        if(!steps.Any())
+            return NotFound();
+        
+        return Ok(steps);
+    }
+}
