e40ffb063352a0393233b8086409bf9ef0a6655e
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-05 23:15:11 +0300

    update controller for generic mapping + add status codes annotations

--- Api/Controllers/Controller.cs
+++ Api/Controllers/Controller.cs
@@ -1,29 +1,34 @@
 using Application.Abstracts;
+using Application.Abstracts.IServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
 
 [ApiController]
-public abstract class Controller<T> : ControllerBase where T : class, IEntity
+public abstract class Controller<TReadDto, TCreateDto, TUpdateDto, TEntity> : ControllerBase
+    where TReadDto : class
+    where TCreateDto : class
+    where TUpdateDto : class, IEntity
+    where TEntity : class, IEntity
 {
-    protected readonly IRepository<T> _repository;
+    protected readonly IService<TReadDto, TCreateDto, TUpdateDto, TEntity> _service;
 
-    public Controller(IRepository<T> repository)
+    public Controller(IService<TReadDto, TCreateDto, TUpdateDto, TEntity> service)
     {
-        _repository = repository;
+        _service = service;
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<T>>> GetAll()
+    public async Task<ActionResult<IEnumerable<TReadDto>>> GetAll()
     {
-        var result = await _repository.GetAllAsync();
+        var result = await _service.GetAllAsync();
         return Ok(result);
     }
 
     [HttpGet("{id:guid}")]
-    public async Task<ActionResult<T>> GetById(Guid id)
+    public async Task<ActionResult<TReadDto>> GetById(Guid id)
     {
-        var entity = await _repository.GetByIdAsync(id);
+        var entity = await _service.GetByIdAsync(id);
         if (entity is null)
         {
             return NotFound();
@@ -32,29 +37,36 @@ public abstract class Controller<T> : ControllerBase where T : class, IEntity
         return Ok(entity);
     }
 
+    
+    [ProducesResponseType(StatusCodes.Status201Created)]
     [HttpPost]
-    public async Task<ActionResult<T>> Create(T entity)
+    public async Task<ActionResult<Guid>> Create(TCreateDto dto)
     {
-        var entityId = await _repository.AddAsync(entity);
+        var entityId = await _service.CreateAsync(dto);
         return CreatedAtAction(nameof(GetById), new { id = entityId }, entityId);
     }
 
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [HttpPut("{id:guid}")]
-    public async Task<ActionResult<T>> Update(Guid id, T entity)
+    public async Task<IActionResult> Update(Guid id, TUpdateDto dto)
     {
-        if (id != entity.Id)
+        if (id != dto.Id)
         {
             return BadRequest();
         }
         
-        await _repository.UpdateAsync(id, entity);
+        await _service.UpdateAsync(id, dto);
         return NoContent();
     }
 
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [HttpDelete("{id:guid}")]
-    public async Task<ActionResult> Delete(Guid id)
+    public async Task<IActionResult> Delete(Guid id)
     {
-        await _repository.DeleteAsync(id);
+        await _service.DeleteAsync(id);
         return NoContent();
     }
 }
