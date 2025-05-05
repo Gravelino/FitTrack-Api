34821d7053f26bd4b8754b3b147c980e7c98ad62
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-06 00:06:47 +0300

    add weights controller

---
+++ Api/Controllers/WeightsInfoController.cs
@@ -0,0 +1,32 @@
+using Application.Abstracts.IServices;
+using Application.DTOs.WeightsInfo;
+using Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class WeightsInfoController : Controller<WeightsInfoReadDto, WeightsInfoCreateDto, WeightsInfoUpdateDto, WeightsInfo>
+{
+    private new readonly IWeightsInfoService _service;
+
+    public WeightsInfoController(IWeightsInfoService service) : base(service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("get-by-userId-and-period/{userId:guid}")]
+    public async Task<ActionResult<IEnumerable<WeightsInfoReadDto>>> GetByUserIdAndPeriod(Guid userId,
+        [FromQuery] DateTime fromDate,
+        [FromQuery] DateTime toDate)
+    {
+        var weights = await _service.GetWeightsInfoByUserIdAndPeriod(userId, fromDate, toDate);
+        if (!weights.Any())
+            return NotFound();
+        
+        return Ok(weights);
+    }
+}
