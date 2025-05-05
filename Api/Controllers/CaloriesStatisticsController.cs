aadbd0f4772c8c9a506ecb36d584baa85a2df944
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-05 23:16:46 +0300

    add controller for calorie statistics

---
+++ Api/Controllers/CaloriesStatisticsController.cs
@@ -0,0 +1,30 @@
+using Application.Abstracts.IServices;
+using Application.DTOs.Meal;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class CaloriesStatisticsController : ControllerBase
+{
+    private readonly ICalorieStatisticsService _calorieStatisticsService;
+
+    public CaloriesStatisticsController(ICalorieStatisticsService calorieStatisticsService)
+    {
+        _calorieStatisticsService = calorieStatisticsService;
+    }
+
+    [HttpGet("{userId:guid}")]
+    public async Task<ActionResult<IEnumerable<CaloriesStatisticsDto>>> GetCaloriesStatistics(
+        Guid userId,
+        [FromQuery] DateTime fromDate,
+        [FromQuery] DateTime toDate,
+        [FromQuery] CaloriesGroupBy groupBy)
+    {
+        var result = await _calorieStatisticsService.GetStatisticsAsync(userId, fromDate, toDate, groupBy);
+        return Ok(result);
+    }
+}
