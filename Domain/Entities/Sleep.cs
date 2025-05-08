3306e057cfe0d3b56a9ba4166160547124e879cd
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-08 13:00:28 +0300

    rename data fields in sleep entity

--- Domain/Entities/Sleep.cs
+++ Domain/Entities/Sleep.cs
@@ -5,8 +5,8 @@ namespace Domain.Entities;
 public class Sleep : IEntity
 {
     public Guid Id { get; set; }
-    public required DateTime From { get; set; }
-    public required DateTime To { get; set; }
+    public required DateTime SleepStart { get; set; }
+    public required DateTime WakeUpTime { get; set; }
     
     public Guid UserId { get; set; }
     public User User { get; set; }
