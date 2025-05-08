04fa1ca81461e39666339c62a2e615a221ff2a76
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-08 13:00:57 +0300

    db migration for sleep entity changes

---
+++ Infrastructure/Migrations/20250507213824_SleepsRenamedDatesField.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SleepsRenamedDatesField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "To",
+                table: "Sleeps",
+                newName: "WakeUpTime");
+
+            migrationBuilder.RenameColumn(
+                name: "From",
+                table: "Sleeps",
+                newName: "SleepStart");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "WakeUpTime",
+                table: "Sleeps",
+                newName: "To");
+
+            migrationBuilder.RenameColumn(
+                name: "SleepStart",
+                table: "Sleeps",
+                newName: "From");
+        }
+    }
+}
