c902b90fcfc255b8c33a3879744bf1204d65ce6e
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-09 20:12:29 +0300

    date bug fix

--- Infrastructure/Repositories/WaterIntakeLogRepository.cs
+++ Infrastructure/Repositories/WaterIntakeLogRepository.cs
@@ -12,6 +12,8 @@ public class WaterIntakeLogRepository : Repository<WaterIntakeLog>, IWaterIntake
 
     public async Task<IEnumerable<WaterIntakeLog>> GetByUserIdAndDayAsync(Guid userId, DateTime date)
     {
+        date = DateTime.SpecifyKind(date, DateTimeKind.Utc);
+        
         var waterIntakeLogs = await _context.WaterIntakeLogs
             .Where(w => w.UserId == userId && date.Date == w.Date)
             .ToListAsync();
