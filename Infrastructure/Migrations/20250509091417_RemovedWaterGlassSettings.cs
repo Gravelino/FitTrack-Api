a8b04f1f6837b7f403046394e8ed16395a84980a
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-09 12:15:36 +0300

    db migration for removing water glass settings

---
+++ Infrastructure/Migrations/20250509091417_RemovedWaterGlassSettings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedWaterGlassSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WaterGlassSettings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WaterGlassSettings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DefaultGlassVolumeMl = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WaterGlassSettings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WaterGlassSettings_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WaterGlassSettings_UserId",
+                table: "WaterGlassSettings",
+                column: "UserId",
+                unique: true);
+        }
+    }
+}
