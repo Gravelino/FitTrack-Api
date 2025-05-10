90567e28443a56f4f0782bed564468bb8eb76850
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-10 22:48:24 +0300

    db migration fro gym updating

---
+++ Infrastructure/Migrations/20250510194723_AddGymRating.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGymRating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RatingCount",
+                table: "Gyms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RatingSum",
+                table: "Gyms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RatingCount",
+                table: "Gyms");
+
+            migrationBuilder.DropColumn(
+                name: "RatingSum",
+                table: "Gyms");
+        }
+    }
+}
