501b0b97d16db734aa6cf2e16a5f7c3b16b5e99e
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-12 15:32:34 +0300

    add gym images db migration

---
+++ Infrastructure/Migrations/20250511185939_AddGymImages.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGymImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MainImageUrl",
+                table: "Gyms",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "GymImages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ImageUrl = table.Column<string>(type: "text", nullable: false),
+                    GymId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GymImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GymImages_Gyms_GymId",
+                        column: x => x.GymId,
+                        principalTable: "Gyms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GymImages_GymId",
+                table: "GymImages",
+                column: "GymId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GymImages");
+
+            migrationBuilder.DropColumn(
+                name: "MainImageUrl",
+                table: "Gyms");
+        }
+    }
+}
