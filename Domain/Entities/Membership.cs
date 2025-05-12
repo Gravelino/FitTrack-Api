aca990aa15e7db2efd49d56267a83f146fbfbda8
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-12 15:37:25 +0300

    add membership entity

---
+++ Domain/Entities/Membership.cs
@@ -0,0 +1,20 @@
+using Application.Abstracts;
+using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class Membership: IEntity
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int? AllowedSessions { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public decimal Price { get; set; }
+    public MembershipType Type { get; set; }
+    
+    public Guid GymId { get; set; }
+    public Gym Gym { get; set; }
+    
+    public ICollection<UserMembership> UserMemberships { get; set; }
+}
