1e31d4915877fbe91330fce29e543fdbcd0872bd
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-12 16:22:07 +0300

    add db migration for updating memberships

---
+++ Infrastructure/Migrations/20250512132012_UpdateMemberships.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMemberships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Memberships");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Memberships");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DurationMonth",
+                table: "Memberships",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DurationMonth",
+                table: "Memberships");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Memberships",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Memberships",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
