c0117e4c41be075ccaf763f663b3387107fb9e1d
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-12 15:33:40 +0300

    add mapping for images

--- Application/Mapping/GymProfile.cs
+++ Application/Mapping/GymProfile.cs
@@ -1,14 +1,37 @@
+using Application.Abstracts.IServices;
+using Application.DTOs;
 using Application.DTOs.Gym;
+using Application.Mapping.Resolvers;
 using Domain.Entities;
 
 namespace Application.Mapping;
 
 public class GymProfile : GenericProfile<GymReadDto, GymCreateDto, GymUpdateDto, Gym>
 {
-    public GymProfile(): base()
+    public GymProfile()
     {
+        CreateMap<GymCreateDto, Gym>()
+            .ForMember(dest => dest.Id, opt =>
+                opt.Ignore())
+            .ForMember(dest => dest.MainImageUrl, opt =>
+                opt.Ignore())
+            .ForMember(dest => dest.Images, opt =>
+                opt.Ignore());
+        
+        CreateMap<GymUpdateDto, Gym>()
+            .ForMember(dest => dest.MainImageUrl, opt => opt.Ignore())
+            .ForMember(dest => dest.Images, opt => opt.Ignore());
+
         CreateMap<Gym, GymReadDto>()
             .ForMember(dest => dest.Rating, opt =>
-                opt.MapFrom(src => src.AverageRating));
+                opt.MapFrom(src => src.AverageRating))
+            .ForMember(dest => dest.MainImagePreSignedUrl, opt => opt.Ignore())
+            .ForMember(dest => dest.Images, opt => opt.Ignore())
+            .ForMember(dest => dest.MainImagePreSignedUrl, opt =>
+                opt.MapFrom<MainImageUrlResolver>())
+            .ForMember(dest => dest.Images, opt =>
+                opt.MapFrom<ImagesResolver>())
+            .ForAllMembers(opt => 
+                opt.Condition((src, dest, srcMember) => srcMember is not null));
     }
 }
