23c019e5c853423104b4ccf9752a9785eda088d8
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-13 17:04:48 +0300

    update user controller

--- Api/Controllers/UserController.cs
+++ Api/Controllers/UserController.cs
@@ -16,57 +16,13 @@ namespace Api.Controllers;
 [Route("api/[controller]")]
 public class UserController : ControllerBase
 {
-    private readonly IOwnerService _ownerService;
-    private readonly IAdminService _adminService;
     private readonly UserManager<User> _userManager;
     private readonly IGymService _gymService;
 
-    public UserController(IOwnerService ownerService, IAdminService adminService, UserManager<User> userManager, IGymService gymService)
+    public UserController(UserManager<User> userManager, IGymService gymService)
     {
-        _ownerService = ownerService;
-        _adminService = adminService;
         _userManager = userManager;
         _gymService = gymService;
-    }
-
-    [Authorize(Roles = IdentityRoleConstants.Admin)]
-    [HttpPost("create-trainer")]
-    public async Task<IActionResult> CreateTrainerAsync([FromBody] CreateStaffDto dto)
-    {
-        if (dto is null)
-        {
-            return BadRequest("Invalid request");
-        }
-
-        try
-        {
-            await _adminService.CreateTrainerAsync(dto);
-            return Ok();
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
-    }
-
-    [Authorize(Roles = IdentityRoleConstants.Owner)]
-    [HttpPost("create-admin")]
-    public async Task<IActionResult> CreateAdminAsync([FromBody] CreateStaffDto dto)
-    {
-        if (dto is null)
-        {
-            return BadRequest("Invalid request");
-        }
-
-        try
-        {
-            await _ownerService.CreateAdminAsync(dto);
-            return Ok();
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
     }
     
     [Authorize(Roles = IdentityRoleConstants.User)]
