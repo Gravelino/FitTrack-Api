22c1452dcb8c92c6be096c5ffec778fb5b0003f9
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-13 17:02:17 +0300

    add admin repo

--- Application/Abstracts/IRepositories/IAdminRepository.cs
+++ Application/Abstracts/IRepositories/IAdminRepository.cs
@@ -4,5 +4,10 @@ namespace Application.Abstracts.IRepositories;
 
 public interface IAdminRepository
 {
+    Task<Admin?> GetByIdAsync(Guid userId);
+    Task<IEnumerable<Admin>> GetAllAsync();
     Task AddAsync(Admin admin);
+    Task UpdateAsync(Admin admin);
+    Task DeleteAsync(Admin admin);
+    Task<IEnumerable<Admin>> GetAdminsByGymIdAsync(Guid gymId);
 }
--- Infrastructure/Repositories/AdminRepository.cs
+++ Infrastructure/Repositories/AdminRepository.cs
@@ -1,5 +1,6 @@
 using Application.Abstracts.IRepositories;
 using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories;
 
@@ -11,9 +12,44 @@ public class AdminRepository : IAdminRepository
     {
         _context = context;
     }
-    
+
+    public async Task<Admin?> GetByIdAsync(Guid userId)
+    {
+        return await _context.Admins
+            .Include(a => a.User)
+            .FirstOrDefaultAsync(a => a.UserId == userId);
+    }
+
+    public async Task<IEnumerable<Admin>> GetAllAsync()
+    {
+        return await _context.Admins
+            .Include(a => a.User)
+            .ToListAsync();
+    }
+
     public async Task AddAsync(Admin admin)
     {
         await _context.Admins.AddAsync(admin);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Admin admin)
+    {
+        _context.Admins.Update(admin);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Admin admin)
+    {
+        _context.Admins.Remove(admin);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Admin>> GetAdminsByGymIdAsync(Guid gymId)
+    {
+        return await _context.Admins
+            .Include(a => a.User)
+            .Where(a => a.GymId == gymId)
+            .ToListAsync();
     }
 }
