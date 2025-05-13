bd3302e8cd1946556e46539390eb053e71471a71
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-13 22:48:30 +0300

    add get methods

--- Infrastructure/Repositories/ProductRepository.cs
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -11,6 +11,24 @@ public class ProductRepository: Repository<Product>, IProductRepository
     {
     }
 
+    public async Task<IEnumerable<Product>> GetAllProductsAsync(string productType)
+    {
+        var products = await _context.Products
+            .Where(p => p.ProductType == Enum.Parse<ProductType>(productType))
+            .ToListAsync();
+        
+        return products;
+    }
+    
+    public async Task<IEnumerable<Product>> GetProductByIdAsync(Guid id, string productType)
+    {
+        var products = await _context.Products
+            .Where(p => p.Id == id && p.ProductType == Enum.Parse<ProductType>(productType))
+            .ToListAsync();
+        
+        return products;
+    }
+    
     public async Task<IEnumerable<Product>> GetByGymIdAsync(Guid gymId, string productType)
     {
         var products = await _context.Products
