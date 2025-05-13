157fe4f1a6495890de655b21227d2ee5727ecc9a
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-13 23:41:50 +0300

    update product mapping

--- Application/Mapping/ProductProfile.cs
+++ Application/Mapping/ProductProfile.cs
@@ -1,12 +1,23 @@
 using Application.DTOs.Product;
 using Domain.Entities;
+using Domain.Enums;
 
 namespace Application.Mapping;
 
 public class ProductProfile: GenericProfile<ProductReadDto, ProductCreateDto, ProductUpdateDto, Product>
 {
-    public ProductProfile():base()
+    public ProductProfile(): base()
     {
+        CreateMap<Product, ProductReadDto>()
+            .ForMember(dest => dest.ProductType, opt
+                => opt.MapFrom(src => src.ProductType.ToString()));
         
+        CreateMap<ProductCreateDto, Product>()
+            .ForMember(dest => dest.ProductType, opt =>
+                opt.MapFrom(src => Enum.Parse<ProductType>(src.ProductType)));
+        
+        CreateMap<ProductUpdateDto, Product>()
+            .ForMember(dest => dest.ProductType, opt =>
+                opt.MapFrom(src => Enum.Parse<ProductType>(src.ProductType)));
     }
 }
