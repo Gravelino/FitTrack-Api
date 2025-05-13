b63717c7d83435a01f6e2afa244ed73abaa80743
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-13 22:51:55 +0300

    add product dtos

---
+++ Application/DTOs/Product/ProductReadDto.cs
@@ -0,0 +1,14 @@
+using Application.Abstracts;
+
+namespace Application.DTOs.Product;
+
+public class ProductReadDto: IEntity
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }     
+    public string Description { get; set; } 
+    public decimal Price { get; set; }  
+    public string? Image { get; set; }
+    public string ProductType { get; set; }
+    public Guid GymId { get; set; }
+}
