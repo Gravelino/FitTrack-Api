f451daacd22dd2b9400cd59ac166de964406766d
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-13 23:36:59 +0300

    update: rename image to imageUrl

--- Application/DTOs/Product/ProductReadDto.cs
+++ Application/DTOs/Product/ProductReadDto.cs
@@ -8,7 +8,7 @@ public class ProductReadDto: IEntity
     public string Name { get; set; }     
     public string Description { get; set; } 
     public decimal Price { get; set; }  
-    public string? Image { get; set; }
+    public string? ImageUrl { get; set; }
     public string ProductType { get; set; }
     public Guid GymId { get; set; }
 }
--- Domain/Entities/Product.cs
+++ Domain/Entities/Product.cs
@@ -9,7 +9,7 @@ public class Product: IEntity
     public string Name { get; set; }     
     public string Description { get; set; } 
     public decimal Price { get; set; }  
-    public string? Image { get; set; }
+    public string? ImageUrl { get; set; }
 
     public Guid GymId { get; set; }
     public Gym? Gym { get; set; }
