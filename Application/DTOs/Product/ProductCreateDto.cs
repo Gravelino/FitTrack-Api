00b5968b5fc3787453d8ed87dd97c9628278522e
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-13 23:20:59 +0300

    remove image url

--- Application/DTOs/Product/ProductCreateDto.cs
+++ Application/DTOs/Product/ProductCreateDto.cs
@@ -5,7 +5,6 @@ public class ProductCreateDto
     public string Name { get; set; }     
     public string Description { get; set; } 
     public decimal Price { get; set; }  
-    public string? Image { get; set; }
     public string ProductType { get; set; }
     public Guid GymId { get; set; }
 }
