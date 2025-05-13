fefb09f1cc43a5d8d0a34796e8db85928f4be84f
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-13 23:56:28 +0300

    add products controller

---
+++ Api/Controllers/ProductsController.cs
@@ -0,0 +1,82 @@
+using Application.Abstracts.IServices;
+using Application.DTOs.Product;
+using Domain.Constants;
+using Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class ProductsController: Controller<ProductReadDto, ProductCreateDto, ProductUpdateDto, Product>
+{
+    private readonly IProductService _service;
+
+    public ProductsController(IProductService service) : base(service)
+    {
+        _service = service;
+    }
+
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [HttpGet("get-by-type/{type}")]
+    public async Task<ActionResult<IEnumerable<ProductReadDto>>> GetByType(string type)
+    {
+        var products = await _service.GetAllProductsAsync(type);
+        if(!products.Any())
+            return NotFound();
+        
+        return Ok(products);
+    }
+    
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [HttpGet("get-by-gymId/{type}")]
+    public async Task<ActionResult<IEnumerable<ProductReadDto>>> GetByGymIdAndType(Guid gymId, string type)
+    {
+        var products = await _service.GetProductsByGymId(gymId, type);
+        if(!products.Any())
+            return NotFound();
+        
+        return Ok(products);
+    }
+    
+    [Authorize(Roles = IdentityRoleConstants.Admin)]
+    [HttpPost("add-service")]
+    public override async Task<ActionResult<Guid>> Create(ProductCreateDto dto)
+        => await base.Create(dto);
+    
+    [Authorize(Roles = IdentityRoleConstants.Admin)]
+    [HttpPut("update-service/{id:guid}")]
+    public override async Task<IActionResult> Update(Guid id, ProductUpdateDto dto)
+        => await base.Update(id, dto);
+    
+    [Authorize(Roles = IdentityRoleConstants.Admin)]
+    public override async Task<IActionResult> Delete(Guid id)
+        => await base.Delete(id);
+
+    [Authorize(Roles = IdentityRoleConstants.Admin)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [HttpPost("add-good")]
+    public async Task<ActionResult<Guid>> AddGood(ProductCreateDto dto, IFormFile file)
+    {
+        var id = await _service.CreateGoodAsync(dto, file);
+        return CreatedAtAction(nameof(GetById), new { Id = id }, id);
+    }
+    
+    [Authorize(Roles = IdentityRoleConstants.Admin)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [HttpPut("update-good/{id:guid}")]
+    public async Task<IActionResult> UpdateGood(Guid id, ProductUpdateDto dto, IFormFile file)
+    {
+        if(id != dto.Id)
+            return BadRequest();
+        
+        await _service.UpdateGoodAsync(id, dto, file);
+        return NoContent();
+    }
+}
