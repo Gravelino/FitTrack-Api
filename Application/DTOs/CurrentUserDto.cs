662a1bebd50045001232e62518ad0b9099f9c878
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-14 13:02:26 +0300

    rename email to login

--- Application/DTOs/CurrentUserDto.cs
+++ Application/DTOs/CurrentUserDto.cs
@@ -3,7 +3,7 @@ namespace Application.DTOs;
 public class CurrentUserDto
 {
     public Guid Id { get; set; } = default!;
-    public string Email { get; set; } = default!;
+    public string Login { get; set; } = default!;
     public string FirstName { get; set; } = default!;
     public string LastName { get; set; } = default!;
     public string? PictureUrl { get; set; }
