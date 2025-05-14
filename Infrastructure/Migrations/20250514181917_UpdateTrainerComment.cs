79fc9b48804d311b9f691c79ec9a15b9654bf501
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-14 23:04:07 +0300

    Rename and extend TrainerComments columns.

---
+++ Infrastructure/Migrations/20250514181917_UpdateTrainerComment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTrainerComment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "TrainerComments",
+                newName: "MealDate");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "TrainerComments",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "TrainerComments");
+
+            migrationBuilder.RenameColumn(
+                name: "MealDate",
+                table: "TrainerComments",
+                newName: "Date");
+        }
+    }
+}
