738abc2b07d83843ef3af6302a7dcef596c6ca00
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-14 23:40:12 +0300

    Add RegistrationsRemaining to GroupTrainings table

---
+++ Infrastructure/Migrations/20250514203943_AddGroupTrainingsRegistrationsRemaining.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGroupTrainingsRegistrationsRemaining : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RegistrationsRemaining",
+                table: "GroupTrainings",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegistrationsRemaining",
+                table: "GroupTrainings");
+        }
+    }
+}
