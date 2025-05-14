0cabeac51b9dd981ecbd49a9c7d43efcc7d26e1f
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-14 13:13:32 +0300

    update mapping

--- Application/Mapping/ProductProfile.cs
+++ Application/Mapping/ProductProfile.cs
@@ -1,4 +1,5 @@
 using Application.DTOs.Product;
+using Application.Mapping.Resolvers;
 using Domain.Entities;
 using Domain.Enums;
 
@@ -10,14 +11,19 @@ public class ProductProfile: GenericProfile<ProductReadDto, ProductCreateDto, Pr
     {
         CreateMap<Product, ProductReadDto>()
             .ForMember(dest => dest.ProductType, opt
-                => opt.MapFrom(src => src.ProductType.ToString()));
+                => opt.MapFrom(src => src.ProductType.ToString()))
+            .ForMember(dest => dest.ImageUrl, opt => opt.Ignore())
+            .ForMember(dest => dest.ImageUrl, opt =>
+                opt.MapFrom<ProductImageResolver>());
         
         CreateMap<ProductCreateDto, Product>()
             .ForMember(dest => dest.ProductType, opt =>
-                opt.MapFrom(src => Enum.Parse<ProductType>(src.ProductType)));
+                opt.MapFrom(src => Enum.Parse<ProductType>(src.ProductType)))
+            .ForMember(dest => dest.ImageUrl, opt => opt.Ignore());
         
         CreateMap<ProductUpdateDto, Product>()
             .ForMember(dest => dest.ProductType, opt =>
-                opt.MapFrom(src => Enum.Parse<ProductType>(src.ProductType)));
+                opt.MapFrom(src => Enum.Parse<ProductType>(src.ProductType)))
+            .ForMember(dest => dest.ImageUrl, opt => opt.Ignore());
     }
 }
