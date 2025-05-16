58fe56f5cb963611880c673d10ed38de4cf8682b
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-05-16 23:06:29 +0300

    Simplify mapping logic in GymProfile configuration

--- Application/Mapping/GymProfile.cs
+++ Application/Mapping/GymProfile.cs
@@ -14,35 +14,28 @@ public class GymProfile : GenericProfile<GymReadDto, GymCreateDto, GymUpdateDto,
             .ForMember(dest => dest.Id, opt =>
                 opt.Ignore())
             .ForMember(dest => dest.MainImageUrl, opt =>
-                opt.Ignore())
-            .ForMember(dest => dest.Images, opt =>
                 opt.Ignore());
-        
+
         CreateMap<GymUpdateDto, Gym>()
-            .ForMember(dest => dest.MainImageUrl, opt => opt.Ignore())
-            .ForMember(dest => dest.Images, opt => opt.Ignore());
+            .ForMember(dest => dest.MainImageUrl, opt =>
+                opt.Ignore());
 
         CreateMap<Gym, GymReadDto>()
             .ForMember(dest => dest.Rating, opt =>
                 opt.MapFrom(src => src.AverageRating))
             .ForMember(dest => dest.MainImagePreSignedUrl, opt => opt.Ignore())
-            .ForMember(dest => dest.Images, opt => opt.Ignore())
             .ForMember(dest => dest.MainImagePreSignedUrl, opt =>
                 opt.MapFrom<MainImageUrlResolver>())
-            .ForMember(dest => dest.Images, opt =>
-                opt.MapFrom<ImagesResolver>())
             .ForAllMembers(opt => 
                 opt.Condition((src, dest, srcMember) => srcMember is not null));
         
         CreateMap<Gym, GymDetailsDto>()
             .ForMember(dest => dest.Rating, opt =>
                 opt.MapFrom(src => src.AverageRating))
-            .ForMember(dest => dest.MainImagePreSignedUrl, opt => opt.Ignore())
-            .ForMember(dest => dest.Images, opt => opt.Ignore())
+            .ForMember(dest => dest.MainImagePreSignedUrl, opt =>
+                opt.Ignore())
             .ForMember(dest => dest.MainImagePreSignedUrl, opt =>
                 opt.MapFrom<MainImageUrlResolver>())
-            .ForMember(dest => dest.Images, opt =>
-                opt.MapFrom<ImagesResolver>())
             .ForAllMembers(opt => 
                 opt.Condition((src, dest, srcMember) => srcMember is not null));
     }
